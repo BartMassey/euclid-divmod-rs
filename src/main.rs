0cae602ca1e8497b9c2be99c50e576d22b8b1a65
Bart Massey <bart.massey@gmail.com>
2024-12-03 16:28:57 -0800

    added --quiet, --all-signs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ extern crate edm;
 
 #[derive(Parser)]
 struct Args {
+    #[arg(long, short, action)]
+    quiet: bool,
+    #[arg(long, name="all-signs", action)]
+    all_signs: bool,
     op: String,
     numerator: f32,
     denominator: f32,
@@ -14,9 +18,37 @@ fn fail(e: Box<dyn std::error::Error>) -> ! {
     std::process::exit(1);
 }
 
+fn report(op: edm::D, n: f32, d: f32, quiet: bool) {
+    let (q, r) = op(n, d);
+    let reconstructed = q * d + r == n;
+    let in_range = r >= 0.0 && r < d.abs();
+    let output = !quiet || !reconstructed || !in_range;
+    if output {
+        print!("{} {} → {} {}", n, d, q, r);
+    }
+    if !reconstructed {
+        print!(" ({}×{}+{}≠{}, d={})", q, d, r, n, n - q * d + r);
+    }
+    if !in_range {
+        let delta = if r < 0.0 {
+            r
+        } else {
+            r - d
+        };
+        print!("({} not in [0..{})), d={})", r, d.abs(), delta);
+    }
+    if output {
+        println!();
+    }
+}
+
 fn main() {
     let args = Args::parse();
     let op = edm::get_op(&args.op).unwrap_or_else(|e| fail(e.into()));
-    let (q, r) = op(args.numerator, args.denominator);
-    println!("{:20?} {:20?}", q, r);
+    report(op, args.numerator, args.denominator, args.quiet);
+    if args.all_signs {
+        report(op, -args.numerator, args.denominator, args.quiet);
+        report(op, args.numerator, -args.denominator, args.quiet);
+        report(op, -args.numerator, -args.denominator, args.quiet);
+    }
 }
