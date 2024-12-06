e6d79be491339cd9a93e9c21b601331b81f05d49
Bart Massey <bart.massey@gmail.com>
2024-12-06 03:11:52 -0800

    added prop; passed smoke test

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod float_utils;
+use float_utils::{Floaty, FloatBits};
 
 use num::{
     rational::BigRational as Q,
@@ -65,6 +66,69 @@ fn divmod_euclid_exact(n0: f32, d0: f32) -> (f32, f32) {
     divmod_euclid_rational(n0, d0, true)
 }
 
+fn divmod_euclid_prop(n0: f32, d0: f32) -> (f32, f32) {
+    let n = n0.float_bits();
+    let d = d0.float_bits();
+
+    match (n, d) {
+        (FloatBits::Value(nf, mut ne), FloatBits::Value(df, mut de)) => {
+            ne += 1;
+            de += 1;
+            eprintln!("n={nf} {ne} d={df} {de}");
+            // Fix sign bits.
+            let sign = (nf ^ df) >> 24;
+            let nf = (nf & !(1 << 24)) << (24 + ne);
+            let df = (df & !(1 << 24)) << (24 + de);
+
+            // Calculate quotient.
+            let mut qf = nf / df;
+            eprintln!("qf={qf}");
+
+            // Align and adjust exponent.
+            let qz = qf.leading_zeros() as i64;
+            let qs = -127 + qz + 24;
+            qf <<= qs;
+            eprintln!("qb={}", (qf >> 24) & 1);
+            let qe = 127 - qz;
+            eprintln!("q={qs} {:06x} {qe}", qf);
+
+            // Hit the floor.
+            qf &= !((1 << (24 - qe)) - 1);
+            eprintln!("qff={}", qf >> (24 - qe));
+
+            // Calculate remainder.
+            let mut rf = (nf - (qf >> (24 - qe)) * df) >> 24;
+            eprintln!("rf={rf}");
+
+            // Align and adjust exponent.
+            let rz = rf.leading_zeros() as i64;
+            rf <<= -127 + rz + 24;
+            eprintln!("rb={}", (rf >> 24) & 1);
+            let re = 127 - rz - 24;
+            eprintln!("r={rz} {rf:03x} {re}");
+
+            // Return floats.
+            let make_float = |x, f, e, s| {
+                eprintln!(
+                    "mf{} {} {:06x} {} {}",
+                    x,
+                    f as f32 * f32::powf(2.0, (e - 24) as f32),
+                    f, 
+                    e,
+                    s,
+                );
+                let bits = ((s as u32) << 31)
+                    | (((e + 127) as u32 & 0xff) << 23)
+                    | ((f >> 1) as u32 & ((1 << 23) - 1));
+                eprintln!("bits={:032b}", bits);
+                f32::from_bits(bits)
+            };
+            (make_float("q", qf, qe, sign), make_float("r", rf, re, 0))
+        }
+        _ => todo!(),
+    }
+}
+
 pub type D = fn(f32, f32) -> (f32, f32);
 
 #[derive(Debug, Error)]
@@ -79,6 +143,7 @@ pub fn get_op(name: &str) -> Result<D, DivModError> {
         "std" => Ok(divmod_euclid_std),
         "exactish" => Ok(divmod_euclid_exactish),
         "exact" => Ok(divmod_euclid_exact),
+        "prop" => Ok(divmod_euclid_prop),
         n => Err(DivModError::Unknown(n.into())),
     }
 }
